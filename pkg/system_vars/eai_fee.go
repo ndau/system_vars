6c13ebaa223c2fd62ca19cf1973998331f9ac363
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-11-02 15:26:00 +0100

    implement text(un)marshaler for EAIFee

--- pkg/system_vars/eai_fee.go
+++ pkg/system_vars/eai_fee.go
@@ -1,8 +1,12 @@
 package system_vars
 
 import (
+	"encoding"
+	"encoding/base64"
+
 	"github.com/oneiro-ndev/ndaumath/pkg/address"
 	math "github.com/oneiro-ndev/ndaumath/pkg/types"
+	"github.com/pkg/errors"
 )
 
 //go:generate msgp -io=0
@@ -26,4 +30,30 @@ type EAIFeeTable []EAIFee
 type EAIFee struct {
 	Fee math.Ndau
 	To  *address.Address
+}
+
+var _ encoding.TextMarshaler = (*EAIFee)(nil)
+var _ encoding.TextUnmarshaler = (*EAIFee)(nil)
+
+// MarshalText implements encoding.TextMarshaler
+func (f EAIFee) MarshalText() ([]byte, error) {
+	bytes, err := f.MarshalMsg(nil)
+	if err != nil {
+		return bytes, errors.Wrap(err, "marshalling bytes")
+	}
+	return []byte(base64.StdEncoding.EncodeToString(bytes)), nil
+}
+
+// UnmarshalText implements encoding.TextUnmarshaler
+func (f *EAIFee) UnmarshalText(text []byte) error {
+	bytes, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return errors.Wrap(err, "decoding b64")
+	}
+	_, err = f.UnmarshalMsg(bytes)
+	if err != nil {
+		return errors.Wrap(err, "unmarshalling bytes")
+	}
+
+	return nil
 }
