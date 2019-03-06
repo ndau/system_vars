094b41131ff59f397a24117937112afeb5adc117
Eric Schmidt <eric.schmidt@oneiro.io>
2019-03-06 13:53:40 -0800

    Add ExchangeEAIScript system variable

---
+++ pkg/system_vars/exchange_eai.go
@@ -0,0 +1,6 @@
+package sv
+
+// ExchangeEAIScriptName is the name of the ExchangeEAIScript system variable
+//
+// This sytem variable stores the chaincode script for calculating EAI rate for exchange accounts.
+const ExchangeEAIScriptName = "ExchangeEAIScript"
