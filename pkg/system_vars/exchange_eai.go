d2bda8df4c437119a30aa88dc9e7a5ec06164a99
Eric Schmidt <eric.schmidt@oneiro.io>
2019-03-07 14:25:59 -0800

    Fix 2% default exchange EAI constant

--- pkg/system_vars/exchange_eai.go
+++ pkg/system_vars/exchange_eai.go
@@ -7,4 +7,4 @@ const ExchangeEAIScriptName = "ExchangeEAIScript"
 
 // In case the system variable isn't present on the blockchain, we use 2% as the default.
 // See commands/cmd/chasm/examples/two_percent.chbin for where this constant comes from.
-const ExchangeEAIScriptDefault = "oAAjgIQeiA=="
+const ExchangeEAIScriptDefault = "oAAlAMgXqASI"
