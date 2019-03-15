56db78b4622a73f0444ba90fb6d9ec272a227c5a
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-03-15 16:03:19 +0100

    add RecordPrice sysvar showing which account can record price

--- pkg/system_vars/sib.go
+++ pkg/system_vars/sib.go
@@ -9,3 +9,34 @@ const SIBScriptName = "SIBScript"
 // blockchain.
 // See https://github.com/oneiro-ndev/chaincode_scripts/blob/e8289c66fd39b0830cbc06066f771d8eafead370/src/sib/sib.chasm
 const SIBScriptDefault = "oAAmABCl1OgADwJGBSYAnGkw3QDDiiAQjwUlAIhSanTBiiUAiFJqdBCPJQCIUmp0CSUAiFJqdEElAIhSanRJJgCcaTDdACUAiFJqdEFGQIg="
+
+// RecordPriceAddressName is the name of the RecordPriceAddress system variable
+//
+// The value contained in this system variable must be of type address.Address
+const RecordPriceAddressName = "RecordPriceAddress"
+
+// RecordPriceOwnershipName is the name of the public ownership key
+const RecordPriceOwnershipName = "RecordPriceOwnership"
+
+// RecordPriceOwnershipPrivateName is the name of the private ownership key
+const RecordPriceOwnershipPrivateName = "RecordPriceOwnershipPrivate"
+
+// RecordPriceValidationName is the name of the public validation key
+const RecordPriceValidationName = "RecordPriceValidation"
+
+// RecordPriceValidationPrivateName is the name of the private validation key
+const RecordPriceValidationPrivateName = "RecordPriceValidationPrivate"
+
+// RecordPrice encapsulates data about the RecordPrice system variables in a structured way.
+var RecordPrice = SysAcct{
+	Name:    "RecordPrice",
+	Address: RecordPriceAddressName,
+	Ownership: Keypair{
+		Public:  RecordPriceOwnershipName,
+		Private: RecordPriceOwnershipPrivateName,
+	},
+	Validation: Keypair{
+		Public:  RecordPriceValidationName,
+		Private: RecordPriceValidationPrivateName,
+	},
+}
