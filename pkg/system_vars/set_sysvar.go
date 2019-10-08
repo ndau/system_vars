0dd3d96124cfaa9451dda0dd3c5d65c7b9987188
Kent Quirk <kentquirk@gmail.com>
2019-10-08 10:10:12 -0400

    Add copyright info and license

--- pkg/system_vars/set_sysvar.go
+++ pkg/system_vars/set_sysvar.go
@@ -1,5 +1,14 @@
 package sv
 
+// ----- ---- --- -- -
+// Copyright 2019 Oneiro NA, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License 2.0 (the "License").  You may not use
+// this file except in compliance with the License.  You can obtain a copy
+// in the file LICENSE in the source distribution or at
+// https://www.apache.org/licenses/LICENSE-2.0.txt
+// - -- --- ---- -----
+
 // SetSysvarAddressName is the name of the SetSysvarAddress system variable
 //
 // The value contained in this system variable must be of type address.Address
